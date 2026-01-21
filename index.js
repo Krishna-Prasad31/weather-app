385306b44e943811c3cc7532f3dba77c5af36951
Krishna-Prasad31 <krishnaprasad0707cr@gmail.com>
2026-01-21 13:23:55 +0530

    Update index.js
    Corrected the api key mistake

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput")
 const card = document.querySelector(".card")
-const apiKey = "c6bc15cfe13383818e81777a3d46c8fb";
+const apiKey = "blah blah"
 
 weatherForm.addEventListener("submit", async event => {
 
